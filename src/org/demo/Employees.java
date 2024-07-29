094eb7baa0ad466ef62745ea9383f3213e099c60
dhananjeyan23 <jeyan.mech7@gmail.com>
2024-07-29 08:13:33 +0530

    opp

--- src/org/demo/Employees.java
+++ src/org/demo/Employees.java
@@ -2,15 +2,20 @@ package org.demo;
 
 public class Employees {
 	String ename1="Subramanian";
+	String ename3="vijaya";
 	//create the 1st method empName
 			private void empName1() {
 			System.out.println("employeename : "+ename1);
 			}
+			private void empName3() {
+				System.out.println("employeename : "+ename3);
+				}
 			//create the main method 
 			public static void main(String[] args) {
 			//classname objrefname = new classname()
 				Employees ei1 = new Employees();
 				ei1.empName1();
+				ei1.empName3();
 
 }
 }
